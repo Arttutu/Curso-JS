8deca5d56152c1fbfc16bb521b60274911028ed7
Arttutu <arthurtt08@gmail.com>
2022-10-25 12:12:55 -0300

    first commit

---
+++ Fundamentos/organização.js
@@ -0,0 +1,13 @@
+// pode ou não conter ; no final da sentença
+// código é orgnizado em sentença.
+console.log ("Sentença de código");
+
+// código é organizado também em blocos de códigos
+{
+    {
+        //no javascript não tem indentação obrigatória.
+        console.log("olá");
+        console.log('mundo!')
+    }
+
+}
