e10de91a5f875b5b2e4e7c788e66e544c0c04ff4
Arthur <arthurtt08@gmail.com>
2022-11-14 17:44:57 -0300

    forEach2

--- array/forEach2.js
+++ array/forEach2.js
@@ -1,5 +1,14 @@
+// Funcionamento de um forEach
+Array.prototype.forEach2 = function (callback) {
+  for (i = 0; i < this.length; i++) {
+    callback(this[i], i, this) //valor, índice, array
+  }
+}
 const aprovado = ["agatha", "aldo", "daniel", "ana"]
 
-aprovado.forEach(function (nome, indice, array) {
+aprovado.forEach2(function (nome, indice, array) {
   console.log(`${indice + 1}) ${nome} `)
+  if (indice == 0) {
+    console.log(array)
+  }
 })
