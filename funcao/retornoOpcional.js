46a9214e1e07b2fece9e94a30f9e2715c78b5a76
Arthur <arthurtt08@gmail.com>
2022-11-16 18:36:46 -0300

    retorno

---
+++ funcao/retornoOpcional.js
@@ -0,0 +1,13 @@
+function area(largura, altura) {
+  const area = largura * altura
+  if (area > 20) {
+    console.log(`valor acima do permitido: ${area} m^2`)
+  } else {
+    return area
+  }
+}
+console.log(area(2, 2))
+console.log(area(2))
+console.log(area()) //não da erro, mas não faz sentido
+console.log(area(2, 2, 2, 2, 2)) // não da erro /mas não faz sentido
+console.log(area(5, 5))
